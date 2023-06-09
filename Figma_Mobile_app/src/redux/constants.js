d64e3f34efbefcd2f28158ff1d833d69b4fa4bea
Tejas Bagul_007 <127741818+Tejasbagul007@users.noreply.github.com>
2023-06-09 10:33:30 +0530

    Delete Figma_Mobile_app directory

--- Figma_Mobile_app/src/redux/constants.js
+++
@@ -1,5 +0,0 @@
-// actionTypes.js
-
-export const SET_USER = "SET_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
-// This constants requires in both actions and reducers in order to maintain decoram;
